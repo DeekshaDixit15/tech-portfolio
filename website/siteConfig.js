026bcd7b1a78206fa6c4acedc8d20a74db52988d
DeekshaDixit15 <43583792+DeekshaDixit15@users.noreply.github.com>
2020-10-02 15:18:18 -0400

    Update siteConfig.js

--- website/siteConfig.js
+++ website/siteConfig.js
@@ -13,9 +13,9 @@ const users = [];
 
 const siteConfig = {
     title: 'Deeksha Dixit', // Title for your website.
-    tagline: 'Continuously improving, aspiring technologist.',
+    tagline: 'Computer Vision | Machine Learning | Robotics',
     profilePhoto: 'img/profile.png',
-    url: 'https://joatmon08.github.io', // Your website URL
+    url: 'https://deekshadixit15.github.io', // Your website URL
     baseUrl: '/tech-portfolio/', // Base URL for your project */
     // For github.io type URLs, you would set the url and baseUrl like:
     //   url: 'https://facebook.github.io',
@@ -23,7 +23,7 @@ const siteConfig = {
 
     // Used for publishing and more
     projectName: 'tech-portfolio',
-    organizationName: 'joatmon08',
+    organizationName: 'University of Maryland, College Park',
     // For top-level user or org sites, the organization is still the same.
     // e.g., for the https://JoelMarcey.github.io site, it would be set like...
     //   organizationName: 'JoelMarcey'
@@ -48,7 +48,7 @@ const siteConfig = {
     },
 
     // This copyright info is used in /core/Footer.js and blog RSS/Atom feeds.
-    copyright: `Copyright © ${new Date().getFullYear()} Your Name Here`,
+    copyright: `Copyright © ${new Date().getFullYear()} Deeksha Dixit`,
 
     highlight: {
         // Highlight.js theme to use for syntax highlighting in code blocks.
@@ -67,7 +67,7 @@ const siteConfig = {
     enableUpdateTime: true,
 
     socialMedia: {
-        linkedin: '',
+        linkedin: 'https://www.linkedin.com/in/d-dixit/',
         twitter: '',
         github: '',
     },
